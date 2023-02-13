af22a8dde90702df0b081fc9f853c93ba4affdb3
Harlan Howe <hhowe7@gatech.edu>
2023-02-13 09:23:22 -0600

    updated documentation

--- src/SudokuRunner.java
+++ src/SudokuRunner.java
@@ -9,14 +9,14 @@ public class SudokuRunner
     /*
     Yeah, this is a pretty blank program. I'd suggest that you...
 
-    Create a 2-d array of integers for your grid:
+    DONE: Create a 2-d array of integers for your grid:
         0 = blank
         -1 through -9 = printed (original) numbers, not to change!
         +1 through +9 = penciled numbers
 
-    Fill in the array with starting numbers... either hard-coded or from a file, if desired.
+    DONE: Fill in the array with starting numbers... either hard-coded or from a file, if desired.
 
-    Write a method draw the grid out. Use absolute values (Math.abs) for numbers and spaces for blanks.
+    DONE: Write a method draw the grid out. Use absolute values (Math.abs) for numbers and spaces for blanks.
 
     Write a method to determine whether the current grid has conflicts in it. Again, use absolute values. (Optionally,
     just worry about the conflicts associated with a given (r, c).)
